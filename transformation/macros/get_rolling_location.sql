b630b17dbc362d79be9ba9c33ff2faba0d1c497d
Van Tran FG <van.tran@fgorg.vn>
2026-01-29 18:30:07 +0700

    fix: resolve dagster concurrency issues and dbt path duplication

--- transformation/macros/get_rolling_location.sql
+++ transformation/macros/get_rolling_location.sql
@@ -1,1 +1,1 @@
-{% macro get_rolling_location() %}{{ env_var('DBT_EXPORT_PATH') }}/rolling/{{ this.name }}/{{ this.name }}_{{ run_started_at.strftime('%Y%m%d%H%M%S') }}.parquet{% endmacro %}
+{% macro get_rolling_location() %}{{ env_var('DBT_EXPORT_PATH') | replace('/rolling', '') }}/rolling/{{ this.name }}/{{ this.name }}_{{ run_started_at.strftime('%Y%m%d%H%M%S') }}.parquet{% endmacro %}
